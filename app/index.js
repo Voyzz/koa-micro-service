a120eacc29a5390ac54bd3068d343844ae4a6ebb
Voyz <18217501371@163.com>
2020-11-04 19:33:41 +0800

    中间件测试

--- app/index.js
+++ app/index.js
@@ -1,11 +1,6 @@
 const Koa = require("koa");
-const bodyParser = require('koa-bodyparser');
 const router = require("./routes");
-
 const app = new Koa();
-
-// 解析请求体
-app.use(bodyParser());
 
 // 路由
 app.use(router.routes()).use(router.allowedMethods());
--- app/utils/urlParamsParse.js
+++ app/utils/urlParamsParse.js
@@ -1,9 +1,10 @@
 module.exports = (url)=>{
     let paramsObj = {};
-
-    !!url && url.split('?')[1].split('&').forEach((param)=>{
-        paramsObj[param.split('=')[0]] = param.split('=')[1];
-    })
+    if(!!url && url.split('?').length > 1){
+        url.split('?')[1].split('&').forEach((param)=>{
+            paramsObj[param.split('=')[0]] = param.split('=')[1];
+        })
+    }
 
     return paramsObj
 }
