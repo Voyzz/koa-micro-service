b9b614d8f9c4d0ac8295631dd88307b9c3bd277e
Voyz <18217501371@163.com>
2020-11-30 18:42:59 +0800

    配置跨域

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const Koa = require("koa");
 const app = new Koa();
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 
 // 解析请求体
 app.use(bodyParser());
@@ -8,5 +9,8 @@ app.use(bodyParser());
 // 路由
 const router = require("./routes");
 app.use(router.routes());
+
+//跨域
+app.use(cors());
 
 app.listen(3000);
